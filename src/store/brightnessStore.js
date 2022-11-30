c648776e57d0908e5d3993db83dfaecc459bf6ec
le anh <leanh232002@gmail.com>
2022-11-30 13:37:32 +0700

    fixed update brightness

--- src/store/brightnessStore.js
+++ src/store/brightnessStore.js
@@ -1,76 +1,79 @@
 import create from "zustand";
 import _ from "lodash";
-export const useBrightnessStore= create((set,get) =>({
+export const useBrightnessStore = create((set, get) => ({
     volume: 0,
     check: false,
     temperature: 1700,
     schedule: true,
-    timeFrom:0.0,
-    timeTo:0.0,
-    changeVolume(value){
+    timeFrom: 0.0,
+    timeTo: 0.0,
+    changeVolume(value) {
         window.brightnessAPI.changeVolume(value);
-        set(()=>({volume: value}));
+        set(() => ({ volume: value }));
     },
-    updateVolume(value){
-        if(get().volume===value) return;
-        set(()=>({volume: value}));
+    updateVolume(value) {
+        if (get().volume === value) return;
+        set(() => ({ volume: value }));
     },
-    changeNightLight(value){
+    changeNightLight(value) {
         window.brightnessAPI.changeNightLight(value);
-        set(()=>({
-            check: value
-        }))
+        set(() => ({
+            check: value,
+        }));
     },
-    updateNightLight(value){
+    updateNightLight(value) {
         // console.log("dada", value);
-        if(get().check===value) return;
-        set(()=>({check: value}));
+        if (get().check === value) return;
+        set(() => ({ check: value }));
     },
-    changeTemperature(value){
+    changeTemperature(value) {
         window.brightnessAPI.changeTemperature(value);
-        set(()=>({
+        set(() => ({
             temperature: value,
-        }))
+        }));
     },
-    updateTemperature(value){
-        if(get().temperature===+value) return;
-        set(()=>({temperature: value}))
+    updateTemperature(value) {
+        if (get().temperature === Number(value)) return;
+
+        set(() => ({ temperature: +value }));
     },
-    changeSchedule(value){
+    changeSchedule(value) {
         window.brightnessAPI.changeSchedule(value);
-        set(()=>({
-            schedule: value
-        }))
-    },
-    updateSchedule(value){
-        if(get().schedule === value) return;
-        set(()=>({
+        set(() => ({
             schedule: value,
-        }))
+        }));
     },
-    updateTimeFrom(value){
-        if(get().timeFrom === value.timeFrom) return;
+    updateSchedule(value) {
+        if (get().schedule === value) return;
         set(() => ({
-            timeFrom: value
-        }))
+            schedule: value,
+        }));
     },
-    changeTimeFrom(value){
+    updateTimeFrom(value) {
+        if (get().timeFrom === value) {
+            return;
+        }
+        set(() => ({
+            timeFrom: value,
+        }));
+    },
+    changeTimeFrom(value) {
         window.brightnessAPI.changeTimeFrom(value);
-        set(()=>({
-            timeFrom: value
-        }))
-    },
-    updateTimeTo(value){
-        if(get().timeTo === value.timeTo) return;
         set(() => ({
-            timeTo: value
-        }))
+            timeFrom: value,
+        }));
     },
-    changeTimeTo(value){
+    updateTimeTo(value) {
+        if (get().timeTo === value) return;
+        set(() => ({
+            timeTo: value,
+        }));
+    },
+    changeTimeTo(value) {
         window.brightnessAPI.changeTimeTo(value);
-        set(()=>({
-            timeTo: value
-        }))
+        set(() => ({
+            timeTo: value,
+        }));
     },
     // setTimeFrom(h, m){
     //     set(() => ({
@@ -80,4 +83,4 @@ export const useBrightnessStore= create((set,get) =>({
     //         }
     //     }))
     // }
-}))
+}));
