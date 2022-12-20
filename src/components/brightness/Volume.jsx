886e8b13ec140d91ba56f91d938e6ebc2f371e44
Duong Bao <duongbao.08012001@gmail.com>
2022-12-20 14:27:46 +0700

    doi cai chi ko biet:)))

--- src/components/brightness/Volume.jsx
+++ src/components/brightness/Volume.jsx
@@ -2,22 +2,11 @@ import { useBrightnessStore } from "../../store/brightnessStore";
 function Volume() {
     const { changeVolume, volume } = useBrightnessStore();
     return (
-        <div
-            className={`max-w-xl mx-auto py-6 px-8 gap-8  rounded-lg shadow-xl flex bg-tr-gradient mt-4 text-white `}
-        >
-            <i className="text-xl ri-sun-line w-7 "></i>
-            <input
-                className="w-full "
-                type="range"
-                id="vol"
-                name="vol"
-                min="0"
-                max="100"
-                value={volume}
-                onChange={(e) => {
-                    changeVolume(e.target.value);
-                }}
-            ></input>
+        <div className={` max-w-2xl mx-auto pt-10 rounded-lg p-6 shadow-xl flex bg-tr-gradientmt-6 `}>
+            <i className="ri-sun-line w-7  "></i>
+            <input className="w-full " type="range" id="vol" name="vol" min="0" max="100"
+            value={(volume)}
+            onChange={(e)=> {changeVolume(e.target.value)}}></input>
         </div>
     );
 }
